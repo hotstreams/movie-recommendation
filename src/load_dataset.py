638bc9852ff20866299a13d435fcf7b9d2980a01
Eddie <hotstreams7@yandex.ru>
2023-10-02 18:37:36 +0300

    test

--- src/load_dataset.py
+++ src/load_dataset.py
@@ -3,10 +3,10 @@ import sys
 from database_service import get_db_connection, create_database, save_raw_data
 
 def load_dataset():
-    movies = pd.read_csv('movies_metadata.csv')
-    ratings = pd.read_csv('ratings_small.csv')
-    keywords = pd.read_csv('keywords.csv')
-    links = pd.read_csv('links_small.csv')
+    movies = pd.read_csv('./resources/movies_metadata.csv')
+    ratings = pd.read_csv('./resources/ratings_small.csv')
+    keywords = pd.read_csv('./resources/keywords.csv')
+    links = pd.read_csv('./resources/links_small.csv')
 
     connection = get_db_connection()
     create_database(connection)
